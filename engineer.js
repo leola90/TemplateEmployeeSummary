a68dc249496b82efa4605297c2bb92e97e6017c0
Leo La <leola90@gmail.com>
2020-03-06 09:47:54 -0600

    initial commit

---
+++ engineer.js
@@ -0,0 +1,12 @@
+const Employee = require("./employee");
+
+class Engineer extends Employee {
+    constructor(name, id, email, gitHub) {
+        super(name, id, email);
+        this.gitHub = gitHub;
+    } 
+
+    getGitHub() {
+        return this.gitHub;
+    }
+}
